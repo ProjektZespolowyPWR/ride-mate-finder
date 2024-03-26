e099d1441f2daa9f7dcbc2eb726d2269121d83fb
Dominik <263915@student.pwr.edu.pl>
2024-03-26 14:36:45 +0100

    initial commit

---
+++ ride-mate-finder-app/src/main/java/ridematefinder/RideMateFinderAppApplication.java
@@ -0,0 +1,13 @@
+package ridematefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideMateFinderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideMateFinderAppApplication.class, args);
+	}
+
+}
