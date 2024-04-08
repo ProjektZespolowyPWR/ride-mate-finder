2875e83a88c945d57afc5905ed178f7385226566
Zbyszek Stasienko <zbyszekstasienko@gmail.com>
2024-04-08 23:22:36 +0200

    add exmaple driver endpoint

---
+++ ride-mate-finder-app/src/main/java/com/ridematefinder/Driver.java
@@ -0,0 +1,12 @@
+package com.ridematefinder;
+
+public class Driver {
+    public int id;
+    public String name;
+    public String description;
+    public Driver(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+}
