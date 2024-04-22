a92ca5fa004a830fd1fd20ad52230dcd35a80e3e
Zbyszek Stasienko <zbyszekstasienko@gmail.com>
2024-04-22 12:29:13 +0200

    db migration to be fixed

--- ride-mate-finder-app/src/main/resources/db/migration/V2__start_database.sql
+++ ride-mate-finder-app/src/main/resources/db/migration/V2__start_database.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS Pictures (
-    picture_id UUID PRIMARY KEY,
+    id UUID PRIMARY KEY,
     picture_path VARCHAR(511) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS "User" (
-    user_id UUID PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS "user" (
+    id UUID PRIMARY KEY,
     driver_id UUID,
-    picture_id UUID REFERENCES Pictures(picture_id),
+    pic_id UUID REFERENCES Pictures(id),
     name VARCHAR(255) NOT NULL,
     surname VARCHAR(255),
     email VARCHAR(255) NOT NULL,
@@ -15,21 +15,22 @@ CREATE TABLE IF NOT EXISTS "User" (
 );
 
 CREATE TABLE IF NOT EXISTS PaymentData (
-    driver_id UUID PRIMARY KEY REFERENCES "User"(user_id),
+    driver_id UUID PRIMARY KEY REFERENCES "user"(id),
     bank_account VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS OpinionUser (
-    user_id_setter UUID REFERENCES "User"(user_id),
-    user_id_receiver UUID REFERENCES "User"(user_id),
+    id UUID PRIMARY KEY,
+    user_id_setter UUID REFERENCES "user"(id),
+    user_id_receiver UUID REFERENCES "user"(id),
     opinion TEXT,
     stars INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS Car (
-    car_id UUID PRIMARY KEY,
-    driver_id UUID REFERENCES "User"(user_id),
-    car_picture_id UUID REFERENCES Pictures(picture_id),
+    id UUID PRIMARY KEY,
+    driver_id UUID REFERENCES "user"(id),
+    car_picture_id UUID REFERENCES Pictures(id),
     vin VARCHAR(255),
     model VARCHAR(255),
     year INTEGER,
@@ -37,16 +38,17 @@ CREATE TABLE IF NOT EXISTS Car (
 );
 
 CREATE TABLE IF NOT EXISTS OpinionCar (
-    user_id_setter UUID REFERENCES "User"(user_id),
-    car_id UUID REFERENCES Car(car_id),
+    id UUID PRIMARY KEY,
+    user_id_setter UUID REFERENCES "user"(id),
+    vehicle_id UUID REFERENCES Car(id),
     opinion TEXT,
     stars INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS Route (
-    route_id UUID PRIMARY KEY,
-    driver_id UUID REFERENCES "User"(user_id),
-    car_id UUID REFERENCES Car(car_id),
+    id UUID PRIMARY KEY,
+    driver_id UUID REFERENCES "user"(id),
+    car_id UUID REFERENCES Car(id),
     start_point VARCHAR(255) ,
     end_point VARCHAR(255),
     date_of_ride DATE,
@@ -55,21 +57,21 @@ CREATE TABLE IF NOT EXISTS Route (
 );
 
 CREATE TABLE IF NOT EXISTS Passengers (
-    route_id UUID REFERENCES Route(route_id),
-    user_id UUID REFERENCES "User"(user_id),
+    id UUID PRIMARY KEY,
+    route_id UUID REFERENCES Route(id),
+    passenger_id UUID REFERENCES "user"(id),
     passenger_spot VARCHAR(255),
-    accepted BIT,
-    PRIMARY KEY (route_id, user_id)
+    accepted BIT
 );
 
 CREATE TABLE IF NOT EXISTS Badges (
-    badge_id UUID PRIMARY KEY,
-    badge_picture_id UUID REFERENCES Pictures(picture_id),
+    id UUID PRIMARY KEY,
+    badge_picture_id UUID REFERENCES Pictures(id),
     badge_description TEXT
 );
 
 CREATE TABLE IF NOT EXISTS UserBadges (
-    user_id UUID REFERENCES "User"(user_id),
-    badge_id UUID REFERENCES Badges(badge_id),
-    PRIMARY KEY (user_id, badge_id)
+    id UUID PRIMARY KEY,
+    client_id UUID REFERENCES "user"(id),
+    badge_id UUID REFERENCES Badges(id)
 );
