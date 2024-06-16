277914e871e9fc1117455a5a823dbbe623a77081
DominikSerwatka <dominikss20002@gmail.com>
2024-06-16 18:02:26 +0200

    final

--- ride-mate-finder-app/src/main/java/com/ridematefinder/repository/PassengersRepository.java
+++ ride-mate-finder-app/src/main/java/com/ridematefinder/repository/PassengersRepository.java
@@ -1,9 +1,12 @@
 package com.ridematefinder.repository;
 
 import com.ridematefinder.sql.Passengers;
+import com.ridematefinder.sql.Route;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PassengersRepository extends JpaRepository<Passengers, UUID> {
+    List<Passengers> findAllByRoute(Route route);
 }
--- ride-mate-finder-app/src/main/java/com/ridematefinder/repository/RouteRepository.java
+++ ride-mate-finder-app/src/main/java/com/ridematefinder/repository/RouteRepository.java
@@ -1,11 +1,14 @@
 package com.ridematefinder.repository;
 
 import com.ridematefinder.sql.Route;
+import com.ridematefinder.sql.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface RouteRepository extends JpaRepository<Route, UUID> {
+    List<Route> findAllByUser(User user);
 }
